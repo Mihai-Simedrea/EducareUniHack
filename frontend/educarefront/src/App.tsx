1393e95aad362866acbaa437534130bb7cc76f45
kaarl77 <60842263+kaarl77@users.noreply.github.com>
2022-11-25 22:57:18 +0200

    Top Cards done

--- frontend/educarefront/src/App.tsx
+++ frontend/educarefront/src/App.tsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UniversityOrFieldCard from "./Components/UniversityOrFieldCard";
+import {CourseCard} from "./Components/CourseCard";
+import {Box} from "@mui/material";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <UniversityOrFieldCard
+                    universityOrFieldName={"Automatica si Calculatoare"}
+                    nrOfExercises={56}
+                    nrOfMaterialsUploaded={123}
+                    nrOfFields={12}
+                    isUniversity={true}
+                />
+                <Box sx={{marginBottom: "16px", marginTop: "16px"}}></Box>
+                <CourseCard
+                    courseAbbreviation={'PTDM'}
+                    onHeartPress={() => {
+                        console.log("hey")
+                    }}
+                    isFavourite={false}
+                    degreeAbbreviation={"CTI"}
+                    fieldName={"Automatica si Calculatoare"}
+                    universityName={"Universitatea Politehnica Timisoara"}
+                    year={2}
+
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
