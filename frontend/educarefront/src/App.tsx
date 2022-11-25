9851a24f2d30463c702490bab4d4d1150d1f2531
kaarl77 <60842263+kaarl77@users.noreply.github.com>
2022-11-26 00:54:40 +0200

    SearchBar done

--- frontend/educarefront/src/App.tsx
+++ frontend/educarefront/src/App.tsx
@@ -1,34 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import UniversityOrFieldCard from "./Components/UniversityOrFieldCard";
 import {CourseCard} from "./Components/CourseCard";
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
+import {SearchBar} from "./Components/SearchBar";
 
 function App() {
+    const [searchedText, setSearchedText] = useState("");
+
     return (
         <div className="App">
             <header className="App-header">
-                <UniversityOrFieldCard
-                    universityOrFieldName={"Automatica si Calculatoare"}
-                    nrOfExercises={56}
-                    nrOfMaterialsUploaded={123}
-                    nrOfFields={12}
-                    isUniversity={true}
-                />
-                <Box sx={{marginBottom: "16px", marginTop: "16px"}}></Box>
-                <CourseCard
-                    courseAbbreviation={'PTDM'}
-                    onHeartPress={() => {
-                        console.log("hey")
-                    }}
-                    isFavourite={false}
-                    degreeAbbreviation={"CTI"}
-                    fieldName={"Automatica si Calculatoare"}
-                    universityName={"Universitatea Politehnica Timisoara"}
-                    year={2}
-
-                />
+                <SearchBar searchedText={searchedText} setSearchedText={setSearchedText}/>
             </header>
         </div>
     );
