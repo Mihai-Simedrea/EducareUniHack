82858015eff9bf8b0bf0a5533fe54b024033905b
adrian-dragos <adrian123795@gmail.com>
2022-11-26 22:28:18 +0200

    idk what is this

---
+++ backend/EducareBE/EducareBE/Migrations/20221126175827_InitialMigration.cs
@@ -0,0 +1,385 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EducareBE.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Universities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TotalSubjects = table.Column<int>(type: "int", nullable: false),
+                    TotalExercices = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Universities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Faculties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UniversityId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Faculties", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Faculties_Universities_UniversityId",
+                        column: x => x.UniversityId,
+                        principalTable: "Universities",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Profiles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UniversityName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FieldName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FacultyName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudyYear = table.Column<int>(type: "int", nullable: false),
+                    NumberOfLikes = table.Column<int>(type: "int", nullable: false),
+                    NumberOfPosts = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Profiles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Fields",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FacultyId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fields", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Fields_Faculties_FacultyId",
+                        column: x => x.FacultyId,
+                        principalTable: "Faculties",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsFavorite = table.Column<bool>(type: "bit", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: true),
+                    FieldId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Courses_Fields_FieldId",
+                        column: x => x.FieldId,
+                        principalTable: "Fields",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EnrolledCourses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CourseId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    IsFavoirte = table.Column<bool>(type: "bit", nullable: true),
+                    IsEnrolled = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnrolledCourses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EnrolledCourses_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EnrolledCourses_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subjects_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubjectsAddedBy",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SubjectId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubjectsAddedBy", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubjectsAddedBy_Subjects_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Likes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    SubjectAddedById = table.Column<int>(type: "int", nullable: false),
+                    LikesCount = table.Column<int>(type: "int", nullable: false),
+                    DislikesCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Likes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Likes_SubjectsAddedBy_SubjectAddedById",
+                        column: x => x.SubjectAddedById,
+                        principalTable: "SubjectsAddedBy",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Likes_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Universities",
+                columns: new[] { "Id", "Name", "TotalExercices", "TotalSubjects" },
+                values: new object[,]
+                {
+                    { 1, "Polytechnic University of Timisoara", 0, 0 },
+                    { 2, "Vest University of Timisoara", 0, 0 },
+                    { 3, "Vest University of Timisoara", 0, 0 },
+                    { 4, "Standford University", 0, 0 },
+                    { 5, "Massachusetts Institute of Technology", 0, 0 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Faculties",
+                columns: new[] { "Id", "Name", "UniversityId" },
+                values: new object[,]
+                {
+                    { 1, "Automation And Computers", 1 },
+                    { 2, "Electronics and Telecomunication", 1 },
+                    { 3, "Faculty Of Managemen And Transportation ", 1 },
+                    { 4, "Faculty of Mechanical Engineerin", 1 },
+                    { 5, "Faculty of Finance and Banking", 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Fields",
+                columns: new[] { "Id", "FacultyId", "Name" },
+                values: new object[] { 1, 1, "Informatics" });
+
+            migrationBuilder.InsertData(
+                table: "Fields",
+                columns: new[] { "Id", "FacultyId", "Name" },
+                values: new object[] { 2, 1, "Computers and Information Technology" });
+
+            migrationBuilder.InsertData(
+                table: "Fields",
+                columns: new[] { "Id", "FacultyId", "Name" },
+                values: new object[] { 3, 2, "Informatics" });
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "FieldId", "IsFavorite", "Name", "Year" },
+                values: new object[] { 1, 2, false, "POO", 2 });
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "FieldId", "IsFavorite", "Name", "Year" },
+                values: new object[] { 2, 2, false, "SDA", 2 });
+
+            migrationBuilder.InsertData(
+                table: "Courses",
+                columns: new[] { "Id", "FieldId", "IsFavorite", "Name", "Year" },
+                values: new object[] { 3, 2, false, "TD", 4 });
+
+            migrationBuilder.InsertData(
+                table: "Subjects",
+                columns: new[] { "Id", "CourseId", "Name" },
+                values: new object[] { 1, 1, "Objects and Classes" });
+
+            migrationBuilder.InsertData(
+                table: "Subjects",
+                columns: new[] { "Id", "CourseId", "Name" },
+                values: new object[] { 2, 1, "Polymorphism" });
+
+            migrationBuilder.InsertData(
+                table: "Subjects",
+                columns: new[] { "Id", "CourseId", "Name" },
+                values: new object[] { 3, 1, "Errors" });
+
+            migrationBuilder.InsertData(
+                table: "SubjectsAddedBy",
+                columns: new[] { "Id", "Name", "SubjectId" },
+                values: new object[,]
+                {
+                    { 1, "Objects", 1 },
+                    { 2, "Classes", 1 },
+                    { 3, "ClassesMaterial", 1 },
+                    { 4, "Poly", 2 },
+                    { 5, "ErrorMaterial", 3 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Courses_FieldId",
+                table: "Courses",
+                column: "FieldId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnrolledCourses_CourseId",
+                table: "EnrolledCourses",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EnrolledCourses_UserId",
+                table: "EnrolledCourses",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Faculties_UniversityId",
+                table: "Faculties",
+                column: "UniversityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Fields_FacultyId",
+                table: "Fields",
+                column: "FacultyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_SubjectAddedById",
+                table: "Likes",
+                column: "SubjectAddedById");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Likes_UserId",
+                table: "Likes",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_UserId",
+                table: "Profiles",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subjects_CourseId",
+                table: "Subjects",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubjectsAddedBy_SubjectId",
+                table: "SubjectsAddedBy",
+                column: "SubjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EnrolledCourses");
+
+            migrationBuilder.DropTable(
+                name: "Likes");
+
+            migrationBuilder.DropTable(
+                name: "Profiles");
+
+            migrationBuilder.DropTable(
+                name: "SubjectsAddedBy");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Fields");
+
+            migrationBuilder.DropTable(
+                name: "Faculties");
+
+            migrationBuilder.DropTable(
+                name: "Universities");
+        }
+    }
+}
