6cba3a818ea60b843b402058010e0cb6074af4ab
kaarl77 <60842263+kaarl77@users.noreply.github.com>
2022-11-26 19:14:49 +0200

    SearchPage WIP

--- frontend/educarefront/src/Components/SearchBar.tsx
+++ frontend/educarefront/src/Components/SearchBar.tsx
@@ -1,30 +1,40 @@
-import {Box, InputAdornment, TextField} from "@mui/material";
+import {Box, InputAdornment, Select, TextField} from "@mui/material";
 import React from "react";
 import {BsSearch} from "react-icons/bs";
 
 interface Props {
     searchedText: string;
     setSearchedText: React.Dispatch<React.SetStateAction<string>>;
+    searchBy: "University" | "Field" | "Courses";
+    fields?: object;
+    years?: object;
 }
 
 export function SearchBar(props: Props) {
-    const {setSearchedText, searchedText} = props;
+    const {setSearchedText, searchedText, searchBy, fields, years} = props;
 
     const styles = {
         container: {
-            height: 128-56,
+            height: 72,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "100vw",
             paddingTop: "56px",
             boxShadow: '0px 0px 10px #7C7C7C',
             borderBottomRightRadius: "20px",
             borderBottomLeftRadius: "20px",
+            position: "fixed",
+            backgroundColor: "white",
+            top: 0,
         },
-    }
+    };
 
     return (
         <Box sx={styles.container}>
             <TextField
                 id="input-with-icon-textfield"
-                label={"Search..."}
+                label={searchBy}
                 InputProps={{
                     endAdornment: (
                         <InputAdornment position={"end"}>
@@ -36,6 +46,16 @@ export function SearchBar(props: Props) {
                     setSearchedText(e.target.value)
                 }}
             />
+            {searchBy === 'Courses' &&
+                <Box sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                    <Select>
+
+                    </Select>
+                    <Select>
+
+                    </Select>
+                </Box>
+            }
         </Box>
     )
 }
