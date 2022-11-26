0239c9badbf923c367efc2f4b9f4cb69dc59ef02
StefanRudareanu <95167214+StefanRudareanu@users.noreply.github.com>
2022-11-27 01:22:49 +0200

    Delete mongo directory

--- mongo/model/coursemodel.js
+++
@@ -1,9 +0,0 @@
-const Connection = require("./connection");
-const Connect = new Connection();
-
-class CourseModel extends Connect {
-  constructor() {
-    super();
-    this.UserSchema = this.mongoose.Schema();
-  }
-}
