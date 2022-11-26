c79d8d93d3c2f52033f7c15d75eda7fa395020a8
kaarl77 <60842263+kaarl77@users.noreply.github.com>
2022-11-26 19:15:32 +0200

    SearchPage WIP

--- frontend/educarefront/src/App.tsx
+++ frontend/educarefront/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import  {BrowserRouter,Routes,Route} from "react-router-dom"
 import LogIn from './Pages/LogIn';
 import Register from './Pages/Register';
 import FillAcount from './Pages/FillAcount';
 import Profile from './Components/Profile';
+import {SearchBar} from "./Components/SearchBar";
+import {SearchPage} from "./Pages/SearchPage";
 function App() {
+  const [inputText,setInputText] = useState("");
   return (
 
     <BrowserRouter>
@@ -14,11 +17,13 @@ function App() {
       <Route path='/register' element={<Register/>}/>
       <Route path='/AccountData' element={<FillAcount/>}/>
       <Route path="/Profile/:emailrouter" element={<Profile/>}/>
+      <Route path = "/Search" element={<SearchPage/>}/>
 
     </Routes>
-    
+
     </BrowserRouter>
-   
+
+    // <SearchPage></SearchPage>
   );
 }
 
