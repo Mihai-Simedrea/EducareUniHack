3e0e8f12f98dd44ded526d9ff7d62491bf78f9f0
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2022-11-26 15:15:16 +0200

    fix error

--- backend/EducareBE/EducareBE/Data/ApplicationDbContext.cs
+++ backend/EducareBE/EducareBE/Data/ApplicationDbContext.cs
@@ -25,7 +25,7 @@ namespace EducareBE.Data
             modelBuilder.Entity<User>()
                 .HasOne(a => a.Profile)
                 .WithOne(b => b.User)
-                .HasForeignKey<Profile>(b => b.UserId);
+                .HasForeignKey<Profile>(b => b.UserEmail);
         }
 
         public DbSet<User> Users { get; set; }
