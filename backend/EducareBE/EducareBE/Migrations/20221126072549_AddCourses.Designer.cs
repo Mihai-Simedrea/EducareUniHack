44d37830b62767d8df955faa11b743f319c18a90
Mihai-Simedrea <mihai.nicolae.simedrea@gmail.com>
2022-11-26 09:27:00 +0200

    Add Migration for Courses

--- backend/EducareBE/EducareBE/Migrations/20221126072549_AddCourses.Designer.cs
+++ backend/EducareBE/EducareBE/Migrations/20221126072549_AddCourses.Designer.cs
@@ -4,6 +4,7 @@ using EducareBE.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EducareBE.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221126072549_AddCourses")]
+    partial class AddCourses
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
